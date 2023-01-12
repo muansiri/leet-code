68f3a726d2e500329ee5e415edde308ccf5b7327
Atthawit Muansiri <atthawit@bangkoklabs.com>
2023-01-12 08:57:05 +0700

    Create 28. Find the Index of the First Occurrence in a String

---
+++ algorithms/28. Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        return haystack.find(needle)
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
