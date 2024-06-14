dc7553e600d88a4099793d4f4b67fbd476b590ee
Atthawit Muansiri <atthawit.mua@gmail.com>
2024-06-14 23:28:07 +0700

    refactor: Use sorting to find the majority element in an array

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -1,9 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        d = {}
-        for num in nums:
-            if num not in d:
-                d[num] = 0
-            d[num] += 1
-            if d[num] > len(nums) / 2:
-                return num
+        nums = sorted(nums)
+        return nums[len(nums) // 2]
